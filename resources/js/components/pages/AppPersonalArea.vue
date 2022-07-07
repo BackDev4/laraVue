1d52c8fa890816c87234b7a230ecd7200277a716
XXX <96119120+Egor-Babashko@users.noreply.github.com>
2022-07-07 18:12:18 +0300

    Страница личный кабинет

---
+++ resources/js/components/pages/AppPersonalArea.vue
@@ -0,0 +1,19 @@
+<template>
+    <the-title>Личный кабинет</the-title>
+</template>
+
+<script>
+
+import TheTitle from "../TheTitle";
+
+export default {
+    name: "AppPersonalArea",
+    components: {
+        TheTitle,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
