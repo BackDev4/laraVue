1f0772a96b91a31c06c0e9ebd3e896894be7453f
XXX <96119120+Egor-Babashko@users.noreply.github.com>
2022-07-10 23:12:42 +0300

    Вывод пользователей

--- resources/js/components/pages/AppUserTable.vue
+++ resources/js/components/pages/AppUserTable.vue
@@ -1,32 +1,41 @@
 <template>
 
-
     <v-card class="deep-purple--text">
         <v-card-title>Страница пользователей</v-card-title>
-        <v-alert></v-alert>
 
-        <v-chip-group
-            class="ps-4"
-            active-class="primary--text"
-            column
-        >
-            <v-chip
-                v-for="tag in tags"
-                :key="tag"
+
+        <div class="text-center pb-5 " v-if="users.length === 0">
+            <v-progress-circular
+                :size="75"
+                :width="7"
+                indeterminate
+                color="deep-purple"
+            ></v-progress-circular>
+        </div>
+
+        <div v-else>
+            <v-chip-group
+                class="ps-4"
+                active-class="primary--text"
+                column
             >
-                <v-icon size="18">{{ tag.icon }}</v-icon>
-                {{ tag.nameBtn }}
-            </v-chip>
-        </v-chip-group>
+                <v-chip
+                    v-for="(tag, index) in tags"
+                    :key="index"
+                >
+                    <v-icon size="18">{{ tag.icon }}</v-icon>
+                    {{ tag.nameBtn }}
+                </v-chip>
+            </v-chip-group>
+            <v-data-table
+                class="elevation-1"
+                :items-per-page="5"
+                :items="users"
+                :headers="headers"
+            >
+            </v-data-table>
+        </div>
 
-        <v-data-table
-            class="elevation-1"
-            :items-per-page="5"
-            :items="users"
-            :headers="headers"
-        >
-
-        </v-data-table>
     </v-card>
 </template>
 
@@ -39,35 +48,31 @@ import {
 
 export default {
     name: "AppUserTable",
+    computed: {},
+    methods: {
+        async getUsers() {
+            await axios.get('api/user').then(response => (this.users = response.data.data));
+        },
+    },
     data() {
         return {
-            users: [
-                {fullName: 'Иванов Иван Иванович', phone: '+375291752356', inn: '3664069397'},
-                {fullName: 'Денисов Денис Денисович', phone: '+375331752342', inn: '3664061394'},
-                {fullName: 'Вадимов Вадим Вадимович', phone: '+375291282659', inn: '2694465697'},
-                {fullName: 'Петрова Наталья Владимировна', phone: '+37544274398', inn: '5543215215'},
-                {fullName: 'Иванов Иван Иванович', phone: '+375291752356', inn: '3664069397'},
-                {fullName: 'Денисов Денис Денисович', phone: '+375331752342', inn: '3664061394'},
-                {fullName: 'Вадимов Вадим Вадимович', phone: '+375291282659', inn: '2694465697'},
-                {fullName: 'Петрова Наталья Владимировна', phone: '+37544274398', inn: '5543215215'},
-                {fullName: 'Иванов Иван Иванович', phone: '+375291752356', inn: '3664069397'},
-                {fullName: 'Денисов Денис Денисович', phone: '+375331752342', inn: '3664061394'},
-                {fullName: 'Вадимов Вадим Вадимович', phone: '+375291282659', inn: '2694465697'},
-                {fullName: 'Петрова Наталья Владимировна', phone: '+37544274398', inn: '5543215215'},
-            ],
+            users: [],
             headers: [
-                {text: 'ФИО', align: 'start', sortable: false, value: 'fullName'},
-                {text: 'Номер телефона', value: 'phone'},
-                {text: 'ИНН', value: 'inn'},
+                { text: 'ФИО', align: 'start', sortable: true, value: 'business_info[0].fullName' },
+                { text: 'Номер телефона', value: 'phone' },
+                { text: 'ИНН', value: 'business_info[0].inn' },
             ],
             tags: [
-                {nameBtn: 'Редактировать', icon: mdiPencil},
+                { nameBtn: 'Редактировать', icon: mdiPencil },
             ],
             icons: {
                 mdiPencil,
                 mdiDelete,
-            }
+            },
         }
+    },
+    mounted() {
+        this.getUsers()
     }
 }
 </script>
